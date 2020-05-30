2306d2ad9b4f849d2507d6e953d1b8dbef848b30
Ricardo Medeiros <ricardo.medeiros.costa@gmail.com>
2020-05-30 16:54:03 -0300

    feat(#4): add documentNumber attribute in Account entity
    Signed-off-by: Ricardo Medeiros <ricardo.medeiros.costa@gmail.com>

--- src/main/java/com/github/ricardomedeirosdacostajunior/transactions/domain/entity/Account.java
+++ src/main/java/com/github/ricardomedeirosdacostajunior/transactions/domain/entity/Account.java
@@ -1,8 +1,17 @@
 package com.github.ricardomedeirosdacostajunior.transactions.domain.entity;
 
+import static lombok.AccessLevel.PRIVATE;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor(force = true, access = PRIVATE)
+@Getter
 public class Account extends BaseEntity {
 
+  @Column(name = "document_number")
+  private final String documentNumber;
 }
--- src/test/java/com/github/ricardomedeirosdacostajunior/transactions/domain/entity/AccountTest.java
+++ src/test/java/com/github/ricardomedeirosdacostajunior/transactions/domain/entity/AccountTest.java
@@ -1,8 +1,13 @@
 package com.github.ricardomedeirosdacostajunior.transactions.domain.entity;
 
+import static com.github.ricardomedeirosdacostajunior.transactions.domain.entity.EntityBaseHelper.getDeclaredField;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import org.junit.jupiter.api.Test;
 
@@ -10,6 +15,17 @@ public class AccountTest {
 
   @Test
   public void accountClassMustBeAnnotatedWithEntityAnnotation() {
-    assertThat( Account.class.isAnnotationPresent( Entity.class ), is( true ) );
+    assertThat(Account.class.isAnnotationPresent(Entity.class), is(true));
+  }
+
+  @Test
+  public void documentNumberMustBeAnnotatedWithColumnAnnotation() {
+    var documentNumberColumnAnnotation =
+        getDeclaredField(Account.class, "documentNumber").getAnnotation(Column.class);
+
+    assertAll(
+        "documentNumberColumnAnnotation",
+        () -> assertThat(documentNumberColumnAnnotation, is(notNullValue())),
+        () -> assertThat(documentNumberColumnAnnotation.name(), is(equalTo("document_number"))));
   }
 }
