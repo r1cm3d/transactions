2a685d67b8f05ebdd028e505d094938c23175c08
Ricardo Medeiros <ricardo.medeiros.costa@gmail.com>
2020-05-30 16:47:32 -0300

    feat(#4): add account class with entity annotation
    Signed-off-by: Ricardo Medeiros <ricardo.medeiros.costa@gmail.com>

---
+++ src/main/java/com/github/ricardomedeirosdacostajunior/transactions/domain/entity/Account.java
@@ -0,0 +1,8 @@
+package com.github.ricardomedeirosdacostajunior.transactions.domain.entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Account extends BaseEntity {
+
+}
---
+++ src/test/java/com/github/ricardomedeirosdacostajunior/transactions/domain/entity/AccountTest.java
@@ -0,0 +1,15 @@
+package com.github.ricardomedeirosdacostajunior.transactions.domain.entity;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import javax.persistence.Entity;
+import org.junit.jupiter.api.Test;
+
+public class AccountTest {
+
+  @Test
+  public void accountClassMustBeAnnotatedWithEntityAnnotation() {
+    assertThat( Account.class.isAnnotationPresent( Entity.class ), is( true ) );
+  }
+}
