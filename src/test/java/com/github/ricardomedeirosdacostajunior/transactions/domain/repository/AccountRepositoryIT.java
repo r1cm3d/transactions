6d7be773db85a873294a3f2d2757fc8efa6cd9a7
Ricardo Medeiros <ricardo.medeiros.costa@gmail.com>
2020-05-31 11:17:47 -0300

    test(#8): add integration tests for accounts find repository. Closes #8
    Signed-off-by: Ricardo Medeiros <ricardo.medeiros.costa@gmail.com>

--- src/test/java/com/github/ricardomedeirosdacostajunior/transactions/domain/repository/AccountRepositoryIT.java
+++ src/test/java/com/github/ricardomedeirosdacostajunior/transactions/domain/repository/AccountRepositoryIT.java
@@ -1,9 +1,11 @@
 package com.github.ricardomedeirosdacostajunior.transactions.domain.repository;
 
 import static java.util.UUID.*;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import com.github.ricardomedeirosdacostajunior.transactions.domain.entity.Account;
 import org.junit.jupiter.api.Test;
@@ -26,5 +28,26 @@ public class AccountRepositoryIT {
     var actualAccount = accountRepository.save(account);
 
     assertThat(actualAccount, is(notNullValue()));
+  }
+
+  @Test
+  public void find() {
+    var expectedUUID = fromString("f080b248-5989-4fc4-80db-07025922bdc9");
+    var expectedDocumentNumber = "06388715907";
+
+    var actualAccount = accountRepository.findById(expectedUUID).orElseThrow();
+
+    assertAll(
+        () -> assertThat(actualAccount.getUuid(), is(equalTo(expectedUUID))),
+        () -> assertThat(actualAccount.getDocumentNumber(), is(equalTo(expectedDocumentNumber))));
+  }
+
+  @Test
+  public void findWhenThereIsNoUUID() {
+    var nonExistentUUID = fromString("93a82d7a-5f59-4032-8942-a9dc9d8a90ce");
+
+    var actualAccount = accountRepository.findById(nonExistentUUID);
+
+    assertThat(actualAccount.isEmpty(), is(true));
   }
 }
---
+++ src/test/resources/data.sql
@@ -0,0 +1,2 @@
+insert into account(uuid, document_number) values
+('f080b248-5989-4fc4-80db-07025922bdc9', '06388715907');
