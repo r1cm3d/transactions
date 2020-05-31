36315566c8d68c1790c24ca54e1a7e24694e694e
Ricardo Medeiros <ricardo.medeiros.costa@gmail.com>
2020-05-31 11:07:39 -0300

    test(#8): add integration tests for accounts find endpoint
    Signed-off-by: Ricardo Medeiros <ricardo.medeiros.costa@gmail.com>

--- src/test/java/com/github/ricardomedeirosdacostajunior/transactions/application/AccountControllerIT.java
+++ src/test/java/com/github/ricardomedeirosdacostajunior/transactions/application/AccountControllerIT.java
@@ -1,11 +1,15 @@
 package com.github.ricardomedeirosdacostajunior.transactions.application;
 
+import static java.lang.String.format;
+import static java.util.UUID.fromString;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.ricardomedeirosdacostajunior.transactions.domain.dto.AccountDTO;
+import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -19,6 +23,7 @@ public class AccountControllerIT {
 
   private static final String DOCUMENT_NUMBER = "42";
   private static final String ACCOUNT_ENDPOINT = "/accounts";
+  private static final UUID aUUID = fromString("1763b508-0331-4c69-a619-9544960594c0");
 
   @MockBean private AccountController accountController;
 
@@ -35,6 +40,13 @@ public class AccountControllerIT {
             post(ACCOUNT_ENDPOINT)
                 .contentType(APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(requestAccountDTO)))
+        .andExpect(status().isOk());
+  }
+
+  @Test
+  public void find() throws Exception {
+    mockMvc
+        .perform(get(format("%s/{uuid}", ACCOUNT_ENDPOINT), aUUID).contentType(APPLICATION_JSON))
         .andExpect(status().isOk());
   }
 }
