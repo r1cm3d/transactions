6a28f682254d2fc337198801cd11d7cab7067446
Ricardo Medeiros <ricardo.medeiros.costa@gmail.com>
2020-05-31 18:41:47 -0300

    test(#6): add test to check entity annotation in transaction entity class
    Signed-off-by: Ricardo Medeiros <ricardo.medeiros.costa@gmail.com>

--- src/main/java/com/github/ricardomedeirosdacostajunior/transactions/domain/entity/Transaction.java
+++ src/main/java/com/github/ricardomedeirosdacostajunior/transactions/domain/entity/Transaction.java
@@ -1,3 +1,15 @@
 package com.github.ricardomedeirosdacostajunior.transactions.domain.entity;
 
-public class Transaction extends BaseEntity {}
+import javax.persistence.Entity;
+
+@Entity
+public class Transaction extends BaseEntity {
+
+//      () -> assertThat(transactionCaptured.getAccount(), is(equalTo(account))),
+//      () -> assertThat(transactionCaptured.getOperationType(), is(equalTo(IN_CASH))),
+//      () -> assertThat(transactionCaptured.getAmount(), is(equalTo(AMOUNT.negate()))),
+//      () -> assertThat(transactionCaptured.getEventDate(), is(notNullValue())),
+//      () -> assertThat(transactionCaptured.getUuid(), is(equalTo(actualTransactionDTO.getAccountUuid()))),
+//      () -> assertThat(transactionCaptured.getEventDate(), is(equalTo(actualTransactionDTO.getEventDate()))));
+
+}
