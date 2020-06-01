f76a1ed363720179e9d2b6ae5976e54b7817251f
Ricardo Medeiros <ricardo.medeiros.costa@gmail.com>
2020-06-01 17:04:02 -0300

    chore(example.sh): add a line break at the end of the file
    Signed-off-by: Ricardo Medeiros <ricardo.medeiros.costa@gmail.com>

--- scripts/shell/example.sh
+++ scripts/shell/example.sh
@@ -7,15 +7,19 @@ NEW_UUID="$(eval $FIRST_REQUEST)";
 echo "Account UUID created"
 echo $NEW_UUID;
 echo "\nQuerying this ID $NEW_UUID"
+
 SECOND_REQUEST="curl -sX GET http://localhost:8080/accounts/$NEW_UUID"
 echo "$SECOND_REQUEST";
 eval "$SECOND_REQUEST";
 echo "\n\nCreating account error example"
+
 THIRD_REQUEST="curl -sX POST http://localhost:8080/accounts -d @scripts/json/account-error.json -H 'Content-type: application/json'";
 echo "$THIRD_REQUEST"
 eval "$THIRD_REQUEST";
 TRANSACTION_REQUEST="{ \"accountUuid\": \"$NEW_UUID\", \"operationType\": \"1\", \"amount\": 123.45 }";
 echo "\n\nCreating a new transaction according this account UUID $NEW_UUID"
+
 FOURTH_REQUEST="curl -sX POST http://localhost:8080/transactions -d '$TRANSACTION_REQUEST' -H 'Content-type: application/json'"
 echo "$FOURTH_REQUEST"
 eval "$FOURTH_REQUEST";
+echo "\n"
