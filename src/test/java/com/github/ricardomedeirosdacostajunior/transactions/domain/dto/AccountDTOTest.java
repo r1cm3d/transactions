3f7af83a45bb6151ece1c90f7df98f77b3b282a5
Ricardo Medeiros <ricardo.medeiros.costa@gmail.com>
2020-06-01 17:22:24 -0300

    extract(#21): extract getField to a private method
    Signed-off-by: Ricardo Medeiros <ricardo.medeiros.costa@gmail.com>

--- src/test/java/com/github/ricardomedeirosdacostajunior/transactions/domain/dto/AccountDTOTest.java
+++ src/test/java/com/github/ricardomedeirosdacostajunior/transactions/domain/dto/AccountDTOTest.java
@@ -8,14 +8,14 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.lang.reflect.Field;
 import org.junit.jupiter.api.Test;
 
 public class AccountDTOTest {
 
   @Test
   public void documentNumberFieldMustBeAnnotatedWithJsonPropertyAnnotation() {
-    var jsonPropertyAnnotation =
-        getDeclaredField(AccountDTO.class, "documentNumber").getAnnotation(JsonProperty.class);
+    var jsonPropertyAnnotation = getField("documentNumber").getAnnotation(JsonProperty.class);
 
     assertAll(
         () -> assertThat(jsonPropertyAnnotation, is(notNullValue())),
@@ -25,12 +25,15 @@ public class AccountDTOTest {
 
   @Test
   public void uuidFieldMustBeAnnotatedWithJsonPropertyAnnotation() {
-    var jsonPropertyAnnotation =
-        getDeclaredField(AccountDTO.class, "uuid").getAnnotation(JsonProperty.class);
+    var jsonPropertyAnnotation = getField("uuid").getAnnotation(JsonProperty.class);
 
     assertAll(
         () -> assertThat(jsonPropertyAnnotation, is(notNullValue())),
         () ->
             assertThat(jsonPropertyAnnotation.value(), is(equalTo("id"))));
+  }
+
+  private Field getField(final String name) {
+    return getDeclaredField(AccountDTO.class, name);
   }
 }
