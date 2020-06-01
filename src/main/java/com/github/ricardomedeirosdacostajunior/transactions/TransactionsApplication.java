6936cdbc7294717749dc9b398fb66ad6db4eff0c
Ricardo Medeiros <ricardo.medeiros.costa@gmail.com>
2020-06-01 06:34:42 -0300

    chore(jacoco): add lombok generated annotation and lombok.config to exclude lombok generated code from jacoco coverage report
    Signed-off-by: Ricardo Medeiros <ricardo.medeiros.costa@gmail.com>

--- src/main/java/com/github/ricardomedeirosdacostajunior/transactions/TransactionsApplication.java
+++ src/main/java/com/github/ricardomedeirosdacostajunior/transactions/TransactionsApplication.java
@@ -1,11 +1,13 @@
 package com.github.ricardomedeirosdacostajunior.transactions;
 
+import lombok.Generated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TransactionsApplication {
 
+  @Generated
   public static void main(String[] args) {
     SpringApplication.run(TransactionsApplication.class, args);
   }
