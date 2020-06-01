4a10d0d6eca82cb2e9eab2250511e585a038fef8
Ricardo Medeiros <ricardo.medeiros.costa@gmail.com>
2020-06-01 14:15:41 -0300

    fix: add RequestBody and TransactionDTO
    Signed-off-by: Ricardo Medeiros <ricardo.medeiros.costa@gmail.com>

--- src/main/java/com/github/ricardomedeirosdacostajunior/transactions/application/controller/AccountController.java
+++ src/main/java/com/github/ricardomedeirosdacostajunior/transactions/application/controller/AccountController.java
@@ -10,6 +10,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +22,7 @@ public class AccountController {
   private final AccountService accountService;
 
   @PostMapping(consumes = APPLICATION_JSON_VALUE)
-  public AccountDTO create(@NotNull final AccountDTO accountDTO) {
+  public AccountDTO create(@NotNull @RequestBody final AccountDTO accountDTO) {
     return accountService.create(accountDTO);
   }
 
