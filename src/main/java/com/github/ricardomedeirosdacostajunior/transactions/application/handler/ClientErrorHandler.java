676e8d559394c4bc60c4b43e9cd79fd9770cf9d3
Ricardo Medeiros <ricardo.medeiros.costa@gmail.com>
2020-06-01 07:13:13 -0300

    feat(#20): change to use bad request exception instead precondition failed
    Signed-off-by: Ricardo Medeiros <ricardo.medeiros.costa@gmail.com>

--- src/main/java/com/github/ricardomedeirosdacostajunior/transactions/application/handler/ClientErrorHandler.java
+++ src/main/java/com/github/ricardomedeirosdacostajunior/transactions/application/handler/ClientErrorHandler.java
@@ -1,13 +1,17 @@
 package com.github.ricardomedeirosdacostajunior.transactions.application.handler;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
 import com.github.ricardomedeirosdacostajunior.transactions.domain.exception.ClientErrorException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ClientErrorHandler {
 
   @ExceptionHandler(ClientErrorException.class)
+  @ResponseStatus(BAD_REQUEST)
   public String clientErrorException(final ClientErrorException clientErrorException) {
     return clientErrorException.getMessage();
   }
--- src/test/java/com/github/ricardomedeirosdacostajunior/transactions/application/handler/ClientErrorHandlerTest.java
+++ src/test/java/com/github/ricardomedeirosdacostajunior/transactions/application/handler/ClientErrorHandlerTest.java
@@ -1,15 +1,18 @@
 package com.github.ricardomedeirosdacostajunior.transactions.application.handler;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasItemInArray;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 import com.github.ricardomedeirosdacostajunior.transactions.ReflectionHelper;
 import com.github.ricardomedeirosdacostajunior.transactions.domain.exception.ClientErrorException;
 import org.junit.jupiter.api.Test;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 public class ClientErrorHandlerTest {
@@ -21,14 +24,25 @@ public class ClientErrorHandlerTest {
 
   @Test
   public void clientErrorExceptionMethodMustBeAnnotatedWithExceptionHandlerAnnotation() {
-    var clientErrorExceptionMethod =
+    var exceptionHandlerAnnotation =
         ReflectionHelper.getDeclaredMethod(ClientErrorHandler.class, "clientErrorException")
             .getAnnotation(ExceptionHandler.class);
 
     assertAll(
-        () -> assertThat(clientErrorExceptionMethod, is(notNullValue())),
+        () -> assertThat(exceptionHandlerAnnotation, is(notNullValue())),
         () ->
             assertThat(
-                clientErrorExceptionMethod.value(), hasItemInArray(ClientErrorException.class)));
+                exceptionHandlerAnnotation.value(), hasItemInArray(ClientErrorException.class)));
+  }
+
+  @Test
+  public void clientErrorExceptionMethodMustBeAnnotatedWithResponseStatusAnnotation() {
+    var responseStatusAnnotation =
+        ReflectionHelper.getDeclaredMethod(ClientErrorHandler.class, "clientErrorException")
+            .getAnnotation(ResponseStatus.class);
+
+    assertAll(
+        () -> assertThat(responseStatusAnnotation, is(notNullValue())),
+        () -> assertThat(responseStatusAnnotation.value(), is(equalTo(BAD_REQUEST))));
   }
 }
