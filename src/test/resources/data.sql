0cda471efaae9bf240f937de2d9603e4ee7858a1
Ricardo Medeiros <ricardo.medeiros.costa@gmail.com>
2020-06-26 11:18:47 -0300

    chore(data.sql): update database to able handle integration tests
    Signed-off-by: Ricardo Medeiros <ricardo.medeiros.costa@gmail.com>

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,2 +1,2 @@
-insert into account(uuid, document_number) values
-('f080b248-5989-4fc4-80db-07025922bdc9', '06388715907');
+insert into account(uuid, document_number, available_credit_limit) values
+('f080b248-5989-4fc4-80db-07025922bdc9', '06388715907', 1000.00);
